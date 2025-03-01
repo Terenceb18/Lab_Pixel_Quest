0124a901cfaeb6876d7716c22ec030258a05e672
Terenceb18 <aiden46.tab@gmail.com>
2025-03-01 11:15:09 -0500

    Geo Quest

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -10,7 +10,8 @@ public class GeoController : MonoBehaviour
 {
     string str = "Hello ";
     string str2;
-    public int newint = 5;
+    public int newint = 6;
+    public int newint2 = 10;
     double new_val = 3.65d;
     private Rigidbody2D rb;
     public string nextLevel = "Scene2";
@@ -31,15 +32,15 @@ public class GeoController : MonoBehaviour
         /*Debug.Log(new_val);
         new_val++;*/
         //transform.position += new Vector3(0.005f, 0, 0);
-        
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            transform.position += new Vector3(0, 1, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            transform.position += new Vector3(0, -1, 0);
-        }
+
+        /* if (Input.GetKeyDown(KeyCode.W))
+         {
+             transform.position += new Vector3(0, 1, 0);
+         }
+         if (Input.GetKeyDown(KeyCode.S))
+         { 
+             transform.position += new Vector3(0, -1, 0);
+         } */
         /*
         if (Input.GetKeyDown(KeyCode.A))
         {
@@ -50,9 +51,11 @@ public class GeoController : MonoBehaviour
             rb.velocity += new Vector2(2, rb.velocity.y);
         }
         */
+        float yInput = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(yInput * newint2, rb.velocity.y);
 
         float xInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(xInput * newint, rb.velocity.y);
+        rb.velocity = new Vector2(xInput * newint, rb.velocity.x);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
